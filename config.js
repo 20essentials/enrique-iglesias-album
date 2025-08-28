bb2ab1c476f15d75951ba55c89dc86197c1d81b9
DominicDev <s0l0estudi4r@gmail.com>
2025-08-28 22:57:14 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'ENRIQUE IGLESIAS ';
+export let nameSongs = [
+	"BAILANDO ", /*1*/
+	"SUBEME LA RADIO", /*2*/
+	"DUELE EL CORAZÓN", /*3*/
+	"EL PERDÓN", /*4*/
+		"EL PERDEDOR", /*5*/
+	"CUANDO ME ENAMORO", /*6*/
+	"HERO", /*7*/
+	"POR AMARTE", /*8*/
+		"SI TU TE VAS", /*9*/
+	"AYER", /*10*/
+	"ALGUIEN SOY YO", /*11*/
+	"LLORO POR TI", /*12*/
+	"NUNCA TE OLVIDARÉ", /*13*/
+	"NO ME DIGAS QUE NO", /*14*/
+	"LOCO", /*15*/
+	"DÍMELO", /*16*/
+	"TONIGHT (I'M LOVIN' YOU)", /*17*/
+	"DIRTY DANCER", /*18*/
+	"FINALLY FOUND YOU", /*19*/
+	"I LIKE IT", /*20*/
+]
